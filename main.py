e26a87b3a2b362f40a4adf46ee12b46659403aa1
MomokaIchi <143499383+MomokaIchi@users.noreply.github.com>
2026-04-05 10:39:13 +0900

    Organized folders and files

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from database import create_db_and_tables
 from routers.users import router as users_router
-from models.user import User
-from models.post import Post
+from routers.posts import router as posts_router
+from routers.auth import router as auth_router
+# from models.user import User
+# from models.post import Post
 
-
+# declaration
 app = FastAPI()
 
+# to ensure existing a database (mainly for the first time)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
 app.include_router(users_router)
+app.include_router(posts_router)
+app.include_router(auth_router)
 
 # http://127.0.0.1:8000
 
--- models/post.py
+++ models/post.py
@@ -6,4 +6,5 @@ class Post(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     mood: str
+    mood_score: int
     created_at: datetime = Field(default_factory=datetime.utcnow)
